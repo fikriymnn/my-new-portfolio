98da5b78d7eaf0ea2af89d6a483b7851fb0e3194
Fikri Zaki Aditama, S.Si <fikriyomann@student.upi.edu>
2024-06-11 11:42:36 +0700

    update

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -7,8 +7,9 @@ const Projects = () => {
     return (
         <div className='"py-20' id='projects'>
             <h1 className='heading'>
-                A small selection of {' '}
-                <span className='text-purple'>recent projects</span>
+                All {' '}
+                <span className='text-purple'>projects {' '}</span>
+                <span>that I have ever worked on</span>
             </h1>
             <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10'>
                 {projects.map(({ id, title, des, img, iconLists, link }) => (
